21319d7480c7d2c31d492e783794a0e451aff6ed
Ana Francisca <anafrancisca.dev@gmail.com>
2024-05-10 11:46:17 -0300

    Parte incial finalizada

---
+++ Fundamentos/sentença.js
@@ -0,0 +1,39 @@
+//Sentença de código
+console.log("Uepaaaaaa");
+console.log("Exemplos de senteças de códigos");
+
+//Bloco de código
+{
+
+    console.log("Isso é um bloco de código")
+}
+
+//Exercício
+
+{
+    console.log("1 2 3 4 5")
+}
+
+{
+   console.log("6 7 8 9 10")
+}
+
+// Trabalhar com dados
+
+console.log(10) //valor literal
+
+var numero = 20; //variável
+console.log(numero)
+
+
+let salario = 4550.22 //number
+console.log(typeof salario)
+
+let estaChovendo = true //boolean
+console.log(typeof estaChovendo)
+
+console.log(typeof "teste")
+
+
+
+
---
+++ Fundamentos/trocaValores.js
@@ -0,0 +1,11 @@
+let a = 7;
+let b = 94;
+
+let aux;
+//Fazer a troca de a com b
+
+aux = a;
+a = b;
+b = aux;
+
+console.log(a, b)
